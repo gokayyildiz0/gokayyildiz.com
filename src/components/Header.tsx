94c4c80254d180459570df3d8a1ba63ccfebba92
Gökay Yıldız <gokayyildiz0@gmail.com>
2024-10-30 03:37:24 +0300

    Refactor Header component to add mobile menu functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import ThemeToogle from "./ThemeToogle";
 
 export default component$(() => {
+    const isMobileMenuOpen = useSignal(false);
+
     return (
         <header class="container flex flex-row mx-auto">
             <a
@@ -13,7 +15,7 @@ export default component$(() => {
                 gokayildiz@icloud.com
             </a>
             <nav class="flex flex-row-reverse flex-1 order-last" aria-label="Main Navigation">
-                <ul class="inline-flex flex-row gap-2" role="list">
+                <ul class=" hidden md:inline-flex flex-row gap-2" role="list">
                     <li class="dark:hover:text-neutral-100 hover:text-neutral-950" role="listitem">
                         <a href="/" aria-label="Home Page">Home</a>
                     </li>
@@ -26,10 +28,37 @@ export default component$(() => {
                         <a href="/blog" aria-label="Blog Page">Blog</a>
                     </li>
                     <li role="listitem">
-                       <ThemeToogle />
+                        <ThemeToogle />
+                    </li>
+                </ul>
+                <ul class=" inline-flex md:hidden flex-row gap-2" role="list">
+                    <li role="listitem">
+                        <button onClick$={() => isMobileMenuOpen.value = true}>Mobile</button>
+                    </li>
+                    <li role="listitem">
+                        <ThemeToogle />
                     </li>
                 </ul>
             </nav>
+            {isMobileMenuOpen.value &&
+                <nav onClick$={() => isMobileMenuOpen.value = false} id="mobile-nav" class="flex justify-center absolute top-0 left-0 dark:bg-neutral-950 bg-neutral-100 w-full h-full p-20">
+                    <ul class="inline-flex flex-col gap-12 text-6xl " role="list">
+                        <li class="dark:hover:text-neutral-100 hover:text-neutral-950" role="listitem">
+                            <a href="/" aria-label="Home Page">Home</a>
+                        </li>
+
+                        <li class="dark:hover:text-neutral-100 hover:text-neutral-950" role="listitem">
+                            <a href="/projects" aria-label="Projects Page">Projects</a>
+                        </li>
+
+                        <li class="dark:hover:text-neutral-100 hover:text-neutral-950" role="listitem">
+                            <a href="/blog" aria-label="Blog Page">Blog</a>
+                        </li>
+
+                    </ul>
+                </nav>
+            }
+
         </header>
     );
 });
